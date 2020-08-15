f5e1e4fcac4d3284f8e587eccca9d6cfb3c33757
Steven G <stevenglab@gmail.com>
2020-08-15 14:22:06 -0700

    Added slider for images. Still need to add buttons on bottom.

--- src/index.js
+++ src/index.js
@@ -38,7 +38,68 @@ const mobileAnimations = (() => {
 
 })();
 
+const imageSlider = (() => {
+    const leftArrow = document.getElementById('left');
+    const rightArrow = document.getElementById('right');
+    const controlDots = document.querySelectorAll('.controlDot');
+    const imageDiv = document.querySelector('.imageSlider');
+    let offset = 0;
+    let imageNumber = 1;
+
+    const getOffset = () => {
+        switch (imageNumber) {
+            case 1: 
+                offset = 0
+                break;
+            case 2: 
+                offset = 50
+                break;
+            case 3:
+                offset = 100
+                break;
+            case 4:
+                offset = 150
+                break;
+            case 5:
+                offset = 200
+                break;
+            case 6:
+                offset = 250
+                break;
+            default:
+                offset = 0;
+        }
+    }
+    const moveLeft = () => {
+        leftArrow.addEventListener('click', () => {
+            imageNumber -= 1;
+            if (imageNumber < 1) {
+                imageNumber = 1;
+            }
+            getOffset();
+            imageDiv.style.transform = `translateX(-${offset}vw)`;
+        });
+    }
+    //We need to update this to make the numbers right to make it move.
+    const moveRight = () => {
+        rightArrow.addEventListener('click', () => {
+            imageNumber += 1;
+            if (imageNumber > 6) {
+                imageNumber = 6;
+            }
+            getOffset();
+            imageDiv.style.transform = `translateX(-${offset}vw)`;
+        });
+    }
+
+    return { moveLeft,
+            moveRight }
+
+})();
+
 animations.dropDown('photosMenu', '.photosDropdown');
 animations.dropDown('storiesMenu', '.storiesDropdown');
 animations.dropDown('theGuysMenu', '.theGuysDropdown');
 mobileAnimations.circlePopOut();
+imageSlider.moveLeft();
+imageSlider.moveRight();
